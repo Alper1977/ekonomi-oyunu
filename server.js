3c68ce480617c1d3a8324dcfd1f185d4ce753bfa
Alper1977 <alpdo77@hotmail.com>
2026-09-04 18:48:28 +0300

    Update print statement from 'Hello' to 'Goodbye'

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const session = require('express-session');
-const SQLiteStore = require('connect-sqlite3')(session);
+const SQLiteStore = require('connect-sqlite3')(session); 
 const Database = require('better-sqlite3');
 const fs = require('fs');
 
