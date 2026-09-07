55653813ff71b2daa1324d5510f049f38c28d090
Alper1977 <alpdo77@hotmail.com>
2026-09-07 16:41:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const fs = require('fs');
 const app = express();
 const server = http.createServer(app); 
 const io = new Server(server); 
-
+ 
 app.use(express.json());
 app.use(express.static(__dirname));
 
