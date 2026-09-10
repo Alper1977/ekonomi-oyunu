f8ed55c03eb82fd8a3ed9fb357cb8cbecfa5095e
Alper1977 <alpdo77@hotmail.com>
2026-09-10 13:41:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const io = new Server(server);
 app.use(express.json());
 app.use(express.static(__dirname));
 
-console.log("Klasördeki dosyalar:", fs.readdirSync(__dirname));
+console.log("Klasördeki dosyalar:", fs.readdirSync(__dirname)); 
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
