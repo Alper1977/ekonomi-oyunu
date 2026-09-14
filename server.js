1964d313e9ef8fd843c5c50ab08626358e014261
Alper1977 <alpdo77@hotmail.com>
2026-09-14 15:28:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+app.set('trust proxy', 1);
 const http = require('http'); 
 const { Server } = require('socket.io'); 
 const session = require('express-session');
